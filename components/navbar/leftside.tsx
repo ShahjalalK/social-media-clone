5f245c11bb9e45d99a57bcd8a6bdd70e3c321af8
shahjalalk <shahjalalkhan215@gmail.com>
2023-06-18 20:43:29 +0600

    nice project

--- components/navbar/leftside.tsx
+++ components/navbar/leftside.tsx
@@ -1,12 +1,44 @@
-import React, { KeyboardEventHandler, useState } from 'react'
+import React, { KeyboardEventHandler, useEffect, useState } from 'react'
 import Image from 'next/image'
 import {AiOutlineSearch} from 'react-icons/ai'
 import Link from 'next/link'
+import { BiSearchAlt2 } from 'react-icons/bi'
+import { useRecoilValue } from 'recoil'
+import { AllUserState, userType } from '@/recoil/userAuthAtom'
+import { useRouter } from 'next/router'
 
 type Props = {}
 
 const LeftSide = (props: Props) => {
-  const [searchShow, setSearchShow] = useState(false)
+  const router = useRouter()
+  const allUserValue = useRecoilValue(AllUserState)
+  const [filterUser, setFilterUser] = useState([])
+
+  const [searchShow, setSearchShow] = useState<boolean>(false)
+  const [drakOver, setDrakOver] = useState<boolean>(false)
+
+  const [search, setSearch] = useState<string>("")
+
+  const handleSearch = () => {
+    if(search !== ''){
+      let searched =  allUserValue.filter((user) => {
+        return Object.values(user).join('').toLocaleLowerCase().includes(search.toLowerCase())
+      })
+  
+      setFilterUser(searched as any)
+
+    }else{
+      setFilterUser(allUserValue as any)
+    }
+   
+  }
+
+  useEffect(() => {
+    let debound = setTimeout(() => {
+      handleSearch()
+    }, 1000)
+    return () => clearTimeout(debound)
+  }, [search])
  
   return (
     <div className="flex  items-center space-x-2">
@@ -21,6 +53,9 @@ const LeftSide = (props: Props) => {
       <AiOutlineSearch className="text-2xl text-gray-400"/>
     </div>
     <input
+    value={search}
+    onChange={(e) => setSearch(e.target.value)}
+    onClick={() => setDrakOver(true)}
    onKeyPress={(e) => {
     let key= e.keyCode || e.which
     console.log(key)
@@ -36,7 +71,51 @@ const LeftSide = (props: Props) => {
     />
    
   </div>
-        
+  
+  <div className={`absolute top-9 lg:top-12 rounded-b -left-1 w-full lg:w-[430px] bg-white shadow z-50 py-3 overflow-hidden ${ drakOver ? "flex" : "hidden"} flex-col space-y-1`}>
+    {search.length === 0 ? 
+
+    <></>
+    : 
+ <div>
+  {filterUser.length === 0 ? 
+  
+  <div className="px-3 capitalize">No results found...</div>
+  :
+
+  filterUser.map((item : userType) => (
+    <div onClick={() => {
+      router.push(`/in/${item.uid}`)
+      setSearchShow(false)
+      setDrakOver(false)
+    }} className="flex items-center space-x-2 flex-grow px-2 py-1 cursor-pointer hover:bg-gray-200">
+    <BiSearchAlt2 className="text-xl" />
+    <div className="flex-grow flex flex-col -space-y-1">
+    <p className="flex-grow capitalize font-medium whitespace-normal text-sm">{item.displayName?.slice(0, 30) || item.email.split("@")[0].slice(0, 30)}...
+    </p>
+    <p className="line-clamp-1 text-xs whitespace-normal">{item.title.slice(0, 60)}...</p>
+    </div>
+    
+    <Image src={item.photoURL} alt='s' width={50} height={50} className="w-8 h-8 object-cover rounded-full" />
+  </div>
+   ))
+
+
+}
+     
+    
+ </div>
+  
+    }
+    
+
+
+
+  </div>
+  <div onClick={() => {
+   
+    setDrakOver(false)
+  }} className={`${drakOver ? "block" : "hidden"} fixed top-14 left-0 w-full h-full bg-black/60 z-40`}></div>  
     </div>
   )
 }
